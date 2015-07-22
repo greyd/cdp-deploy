bed652e1359dc02f986ebc64c94dd82e3d29b9ba
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-07-22 10:55:29 +0300

    Change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CDP' });
+  res.render('index', { title: 'CDP 123123' });
 });
 
 module.exports = router;
