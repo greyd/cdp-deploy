3b24ce7745a11c08cb9d8755bffad2a9492bf3fe
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-11-19 11:01:52 +0200

    Make some change to trigger new deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CDP' });
+  res.render('index', { title: 'CDP it should work!!!' });
 });
 
 module.exports = router;
